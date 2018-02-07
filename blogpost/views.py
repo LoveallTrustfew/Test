4b36b696607cf41e07d011aec102eef009670033
LoveallTrustfew <549126905@qq.com>
2018-02-07 20:18:36 +0800

    last for today

--- blogpost/views.py
+++ blogpost/views.py
@@ -15,4 +15,4 @@ def index(request):
 def view_post(request, slug):
     return render_to_response('blogpost_detail.html', {
         'post': get_object_or_404(Blogpost, slug=slug)
-    })
+                                              })
