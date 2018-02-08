834974c486f07cd499d9b03534fd3ce788b8a8d4
LoveallTrustfew <549126905@qq.com>
2018-02-08 11:50:37 +0800

    django test

--- blogpost/views.py
+++ blogpost/views.py
@@ -16,3 +16,6 @@ def view_post(request, slug):
     return render_to_response('blogpost_detail.html', {
         'post': get_object_or_404(Blogpost, slug=slug)
                                               })
+
+
+
